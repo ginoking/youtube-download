0e1c2401f71eb6d48c482375415e00eaf6512df7
金沅禹 <gt10245@greattree.com.tw>
2025-03-05 15:50:31 +0800

    fix: 換套件

--- routes/download.js
+++ routes/download.js
@@ -1,84 +1,40 @@
 var express = require('express');
+const https = require("https");
 var router = express.Router();
 const fs = require('fs');
-// const { Downloader } = require('ytdl-mp3');
-
-const ytdl = require('@distube/ytdl-core');
+const youtubedl = require('youtube-dl-exec')
 
 router.post('/download', async function (req, res) {
-    // res.send(req.body.url);
-    // const downloader = new Downloader({
-    //     outputDir: './',
-    //     getTags: true
-    // });
-    // await downloader.downloadSong(req.body.url);
-    const output = 'audio.mp3'; // 输出文件名
-    const stream = ytdl(
-        'https://www.youtube.com/watch?v=UQrP7l2AAVk&list=RDUQrP7l2AAVk', 
-        { filter: 'audioonly', dlChunkSize: 50 },
-    ).pipe(fs.createWriteStream(output));
 
-    ytdl.getBasicInfo("https://www.youtube.com/watch?v=UQrP7l2AAVk&list=RDUQrP7l2AAVk").then(info => {
-        console.log(info.videoDetails.title);
-    });
-
-    // const stream = ytdl(req.body.url, { filter: 'audioonly' }) // 只提取音频
-    //     .pipe(fs.createWriteStream(output));
-
-    // stream.on('drain', (info) => {
-    //     console.log('下载进度: ' + info.videoDetails.title);
-    // });
-
-    stream.on('open', () => {
-        console.log('下載開始');
-    });
-
-    stream.on('data', (chunk) => {
-        console.log('下载进度: ' + chunk.length);
-    });
-
-    stream.on('error', (err) => {
-        console.log('下载失败: ' + err.message);
-    });
-
-    stream.on('close', () => {
-        console.log('下载结束');
-    });
-
-    stream.on('drain', () => {
-        console.log('下载进度drain: ' + stream.bytesRead);
-    });
-
-    stream.on('pipe', () => {
-        console.log('下载进度pipe: ' + stream.bytesRead);
-    });
-
-    stream.on('ready', () => {
-        console.log('下载进度ready: ' + stream.bytesRead);
-    });
-
-    stream.on('unpipe', () => {
-        console.log('下载进度unpipe: ' + stream.bytesRead);
-    });
-
-    stream.on('finish', () => {
-        // res.send('MP3 下载完成');
-        // 下载完成后，提供下载链接
-        res.download(output, 'downloaded_audio.mp3', (err) => {
-            if (err) {
-                res.status(500).send('下载失败: ' + err.message);
-            }
-            // 可选：下载完成后删除文件
-            fs.unlink(output, (err) => {
-                if (err) console.error('无法删除文件: ' + err.message);
-            });
+    youtubedl(req.body.url, {
+        audioQuality: 0,
+        dumpSingleJson: true,
+        noCheckCertificates: true,
+        noWarnings: true,
+        extractAudio: true,
+        audioFormat: 'm4a',
+        format: 'bestaudio[ext=m4a]',
+        addHeader: ['referer:youtube.com', 'user-agent:googlebot'],
+        output: '%(title)s.%(ext)s',
+        httpChunkSize: '1M',
+        postprocessorArgs: ['-threads', '4'],
+        externalDownloader: 'aria2c',
+        externalDownloaderArgs: ['--max-connection-per-server=16', '--split=16', '--min-split-size=1M', '--max-concurrent-downloads=16']
+    }).then(info => {
+        https.get(info.url, function (file) {
+            requestedDownloads = info.requested_downloads
+            downloadOption = requestedDownloads[0]
+            res.set('Content-disposition', 'attachment; filename=' + encodeURI(downloadOption.filename));
+            res.set('Content-Type', 'audio/m4a');
+            file.pipe(res);
         });
-    });
-
-    stream.on('error', (err) => {
-        res.status(500).send('下载失败: ' + err.message);
-    });
-    // res.send('download post');
+    }).catch(err => {
+        console.error('下載錯誤:', err); // 打印錯誤信息
+        console.error('錯誤類型:', err.name); // 打印錯誤類型
+        console.error('錯誤消息:', err.message); // 打印錯誤消息
+        console.error('錯誤堆棧:', err.stack); // 打印錯誤堆棧
+        res.status(500).send(err);
+    })
 });
 
 router.get('/download', function (req, res, next) {
