eb9ed1dab658e75e9a9acc96893a0281ba909ef1
金沅禹 <gt10245@greattree.com.tw>
2025-03-06 17:12:28 +0800

    fix: default port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var downloadRouter = require('./routes/download');
 
 var app = express();
 
-const port = process.env.PORT || 80 ;
+const port = process.env.PORT || 8080 ;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
